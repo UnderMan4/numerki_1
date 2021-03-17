9ed9f5807f12d6aaf9957003df3242584a134b3d
Dynia298 <dynia298@o2.pl>
2021-03-17 12:27:33 +0100

    Dodano funkcje

--- src/App.java
+++ src/App.java
@@ -1,7 +1,18 @@
 public class App {
 
+    private static Function function_a = (x) -> {
+        return 0.5*x*x*x - 3*x*x + 5*x + 3;
+    };
+    private static Function function_b = (x) -> {
+        return Math.pow(5, x);
+    };
+    private static Function function_c = (x) -> {
+        return Math.sin(x);
+    };
+
     public static void main(String args[]) {
         System.out.println("Prosze państwa, tutaj powstanie coś pięknego");
+        System.out.println(function_b.calculate(1.5));
     }
 
 }
--- src/Function.java
+++ src/Function.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public interface Function {
+public interface Function {
+
+    public double calculate(double x);
+
 }
