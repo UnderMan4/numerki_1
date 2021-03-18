418d1b26f934230e7b4a0d44059e4b792b7c9e04
Dynia298 <dynia298@o2.pl>
2021-03-18 23:57:40 +0100

    Dodano metodę siecznej oraz rysowanie funkcji
    Dodano metode siecznycznych.
    Dodano w App metodę wyświetlającą daną funkcję w GNUPlot

--- src/App.java
+++ src/App.java
@@ -1,18 +1,62 @@
+import com.panayotis.gnuplot.JavaPlot;
+import com.panayotis.gnuplot.plot.DataSetPlot;
+import com.panayotis.gnuplot.plot.FunctionPlot;
+import com.panayotis.gnuplot.style.NamedPlotColor;
+import com.panayotis.gnuplot.style.PlotStyle;
+import com.panayotis.gnuplot.style.Style;
+
 public class App {
 
     private static Function function_a = (x) -> {
         return 0.5*x*x*x - 3*x*x + 5*x + 3;
     };
     private static Function function_b = (x) -> {
-        return Math.pow(5, x);
+        return Math.pow(5, x) - 0.5;
     };
     private static Function function_c = (x) -> {
-        return Math.sin(x);
+        return Math.cos(x);
     };
 
+    private static void plot(String fun, double x0, double x_g1, double x_g2) {
+        JavaPlot p = new JavaPlot(false);
+
+        PlotStyle zeroStyle = new PlotStyle();
+        zeroStyle.setStyle(Style.LINES);
+        zeroStyle.setLineType(NamedPlotColor.BLACK);
+        zeroStyle.setLineWidth(2);
+        FunctionPlot zeroLine = new FunctionPlot("0");
+        zeroLine.setPlotStyle(zeroStyle);
+        p.addPlot(zeroLine);
+
+        PlotStyle lineStyle = new PlotStyle();
+        lineStyle.setStyle(Style.LINES);
+        lineStyle.setLineType(NamedPlotColor.BLUE);
+        lineStyle.setLineWidth(4);
+        FunctionPlot functionLine = new FunctionPlot(fun);
+        functionLine.setPlotStyle(lineStyle);
+        p.addPlot(functionLine);
+        p.getAxis("x").setBoundaries(x_g1, x_g2);
+
+        PlotStyle pointStyle = new PlotStyle();
+        pointStyle.setStyle(Style.DOTS);
+        pointStyle.setLineType(NamedPlotColor.RED);
+        pointStyle.setLineWidth(10);
+        double[][] plot = {{x0, 0}};
+        DataSetPlot points = new DataSetPlot(plot);
+        points.setPlotStyle(pointStyle);
+        p.addPlot(points);
+
+        p.set("nokey", "");
+        p.plot();
+    }
+
     public static void main(String args[]) {
-        System.out.println("Prosze państwa, tutaj powstanie coś pięknego");
-        System.out.println(function_b.calculate(1.5));
+
+        double x_g1 = -5, x_g2 = 1;
+        double x0_sieczna_a = Sieczna.sieczna(function_b,-5, 1, 0.0001, true);
+        plot("0.5*x**3 - 3*x**2 + 5*x + 3", x0_sieczna_a, x_g1, x_g2);
+
     }
 
 }
+
---
+++ src/Sieczna.java
@@ -0,0 +1,64 @@
+public class Sieczna {
+
+    public static double sieczna(Function fun, double x_g1, double x_g2, double war, boolean kryt) {
+
+        // Jeżeli kryt == true to uzywamy kryterium stopu bazujacego na epsilonie, jezeli false, to na iteracjach
+
+        double y_g1 = fun.calculate(x_g1);
+        double y_g2 = fun.calculate(x_g2);
+        int iter = 0;
+
+        double a, b, x0 = 0, y0, epsi, prev_x = 0;
+        boolean stop = false;
+
+        do {
+
+            if (kryt) {
+                prev_x = x0;
+            }
+
+            a = (y_g2 - y_g1) / (x_g2 - x_g1);
+            b = y_g1 - (a * x_g1);
+
+            if (a == 0) {
+                return 0;
+            }
+
+            x0 = (0 - b) / a;
+            y0 = (a * x0) + b;
+
+            if ((y0 * y_g1) > 0) {
+                x_g2 = x0;
+                y_g2 = fun.calculate(x_g2);
+            } else {
+                x_g1 = x0;
+                y_g1 = fun.calculate(x_g1);
+            }
+
+            if (x0 == 0) {
+                stop = true;
+            } else {
+                if (kryt) {
+                    epsi = prev_x - x0;
+                    if (epsi < 0) {
+                        epsi = 0 - epsi;
+                    }
+                    if (epsi < war) {
+                        stop = true;
+                    }
+                } else {
+                    war--;
+                    if (war <= 0) {
+                        stop = true;
+                    }
+                }
+            }
+            iter++;
+        } while (stop != true);
+
+        System.out.println("Ilość iteracji dla metody siecznych: " + Integer.toString(iter));
+
+        return x0;
+    }
+
+}
